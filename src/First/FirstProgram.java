9b3fd251633f259b031b5096d0a805d5c6cb5b87
sanat <sanatrathore1214@gmail.com>
2023-02-07 22:22:02 +0530

    first commits

---
+++ src/First/FirstProgram.java
@@ -0,0 +1,9 @@
+package First;
+
+public class FirstProgram {
+	public static void main(String args[])
+	{
+		System.out.println("hello");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author SANAT
+ *
+ */
+module Learn {
+}
